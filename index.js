0c4007cc6922e9819a05c57ad71d1d9beaaa7d5a
Brian Dunstan <bdnstn@gmail.com>
2022-09-02 13:13:07 +0800

    Set development parameters

--- index.js
+++ index.js
@@ -113,7 +113,6 @@ function started(){
     // Send a NOOP to the Cgate server to keep the connection open and detect if the connection was closed
     setInterval(function(){
       if (logging == true) {console.log('NOOP')};
-      command.write('NOOP\n');
     },KEEPALIVE);
   }
 
