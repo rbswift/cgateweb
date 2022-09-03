4ff9b087b4e5a2d6b6dacc97cc443c78d599fe3d
Brian Dunstan <bdnstn@gmail.com>
2022-09-03 14:50:00 +0800

    Unwind accidental inclusions in PR
    Sorry for the noise. I didn't realize my commits were all being included in the PR #1. I should have created a branch.

--- settings.js
+++ settings.js
@@ -4,26 +4,26 @@ exports.cbusip = '127.0.0.1';
 
 
 //cbus project name
-exports.cbusname = "BURS1303";
+exports.cbusname = "HOME";
 
 //mqtt server ip:port
 exports.mqtt = '127.0.0.1:1883';
 
 //username and password (unncomment to use)
-exports.mqttusername = 'HA-BursMQTT';
-exports.mqttpassword = '6M&4DJMg';
+//exports.mqttusername = 'user1';
+//exports.mqttpassword = 'password1';
 
 // net and app for automatically requesting values
-exports.getallnetapp = '254/56';
+// exports.getallnetapp = '254/56';
 
 // whether to request on start (requires getallnetapp set as well)
-exports.getallonstart = true;
+// exports.getallonstart = true;
 
 // how often to request after start (in seconds), (requires getallnetapp set as well)
-exports.getallperiod = 60*60;
+// exports.getallperiod = 60*60;
 
 // Sets MQTT retain flag for values coming from cgate
-exports.retainreads = true;
+// exports.retainreads = true;
 
 exports.messageinterval = 0;
 
