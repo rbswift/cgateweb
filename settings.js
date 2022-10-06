8b3c02392e88674974e61a612c83a792eb892822
Brian Dunstan <bdnstn@gmail.com>
2022-10-06 16:25:39 +0800

    Update mqtt version number to 4.3.7
    update mqtt version in package.json - old version number was causing null object error on server id.

--- settings.js
+++ settings.js
@@ -4,26 +4,26 @@ exports.cbusip = '127.0.0.1';
 
 
 //cbus project name
-exports.cbusname = "HOME";
+exports.cbusname = "BURS1303";
 
 //mqtt server ip:port
 exports.mqtt = '127.0.0.1:1883';
 
 //username and password (unncomment to use)
-//exports.mqttusername = 'user1';
-//exports.mqttpassword = 'password1';
+exports.mqttusername = 'HA-BursMQTT';
+exports.mqttpassword = 'TW0Yo2DnO!7VzjAvoijttpn%EeNZ3o';
 
 // net and app for automatically requesting values
-// exports.getallnetapp = '254/56';
+exports.getallnetapp = '254/56';
 
 // whether to request on start (requires getallnetapp set as well)
-// exports.getallonstart = true;
+exports.getallonstart = true;
 
 // how often to request after start (in seconds), (requires getallnetapp set as well)
-// exports.getallperiod = 60*60;
+exports.getallperiod = 60*60;
 
 // Sets MQTT retain flag for values coming from cgate
-// exports.retainreads = true;
+exports.retainreads = true;
 
 exports.messageinterval = 0;
 
